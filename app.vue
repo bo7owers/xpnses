2082a313bb299066b9e8e1c44f17bb734eab8a56
bo7owers <rene7orres@gmail.com>
2022-07-28 22:40:15 -0400

    Work on hero header

--- app.vue
+++ app.vue
@@ -1,16 +1,5 @@
 <template>
   <div>
-    <h1>Hello there</h1>
-    <h2>h2</h2>
-    <h3>h3</h3>
-    <h4>h4</h4>
-    <h5>h5</h5>
-    <h6>h6</h6>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, iure
-      odit. Vitae aliquid laboriosam ad possimus iste suscipit voluptatem
-      molestiae sunt illum est tempora sed cupiditate facere architecto,
-      eligendi asperiores?
-    </p>
+    <Hero />
   </div>
 </template>
---
+++ components/hero.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+console.log(
+  "Hero Image by Olia Danilevich https://www.pexels.com/photo/back-to-school-flatlay-5088007/"
+);
+</script>
+<template>
+  <header class="hero-header">
+    <img src="../assets/imgs/hero.jpg" alt="" class="hero-img" />
+    <h1>Xpnses, the only way to track your spending</h1>
+  </header>
+</template>
+<style lang="scss">
+header.hero-header {
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  .hero-img {
+    width: 100vw;
+    height: 35vh;
+    object-fit: cover;
+    z-index: 50;
+  }
+  h1 {
+    position: absolute;
+    top: 25%;
+    z-index: 100;
+    color: hsl(0, 0%, 100%);
+
+    &::before {
+      content: "";
+      display: block;
+      position: absolute;
+      width: 100vw;
+      height: 35vh;
+      background-color: hsl(204, 100%, 25%);
+      opacity: 0.3;
+      top: -85%;
+      z-index: 0;
+    }
+  }
+}
+</style>
