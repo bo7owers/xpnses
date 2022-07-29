d6bcc899dca8aaa7b8b0e74aeb87e87b30df9999
bo7owers <rene7orres@gmail.com>
2022-07-29 08:37:20 -0400

    Style hero

--- components/hero.vue
+++ components/hero.vue
@@ -6,40 +6,47 @@ console.log(
 <template>
   <header class="hero-header">
     <img src="../assets/imgs/hero.jpg" alt="" class="hero-img" />
-    <h1>Xpnses, the only way to track your spending</h1>
+    <h1>
+      <span class="brand">Xpnses</span>, the only way to track your spending
+    </h1>
   </header>
 </template>
 <style lang="scss">
+@use "../assets/scss/utils/mixins/breakpoints" as b;
 header.hero-header {
   display: flex;
   flex-flow: column wrap;
   justify-content: center;
   align-items: center;
   position: relative;
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100vw;
+    height: 35vh;
+    background-color: hsl(202, 98%, 36%);
+    opacity: 0.3;
+    left: 0;
+  }
   .hero-img {
     width: 100vw;
     height: 35vh;
     object-fit: cover;
-    z-index: 50;
+    filter: blur(2px);
+    opacity: 0.6;
   }
   h1 {
     position: absolute;
     top: 25%;
-    z-index: 100;
-    color: hsl(0, 0%, 100%);
-    padding: 0 1.25rem;
-
-    &::before {
-      content: "";
-      display: block;
-      position: absolute;
-      width: 100vw;
-      height: 35vh;
-      background-color: hsl(204, 100%, 25%);
-      opacity: 0.3;
-      top: -57%;
-      left: 0;
-      z-index: 0;
+    color: hsl(0, 0%, 0%);
+    padding: 0.25rem 0rem 0.25rem 0.5rem;
+    @include b.breakpoint(small) {
+      padding: 2rem 3rem;
+    }
+    @include b.breakpoint(medium) {
+      padding: 0 1.25rem;
+      max-width: 45vw;
     }
   }
 }
