2b38cbc74c317e4e909a385b3c4ce583b80dfee0
bo7owers <rene7orres@gmail.com>
2022-08-02 21:16:27 -0400

    Remove store (not needed yet)

--- app.vue
+++ app.vue
@@ -1,35 +1,8 @@
-<script setup lang="ts">
-import { ref, onMounted, watch } from "vue";
-import { useUNameStore } from "@/stores/index";
-
-const name = ref<string>();
-
-watch(name, (newName) => {
-  localStorage.setItem("userName", newName);
-});
-
-onMounted(() => {
-  name.value = localStorage.getItem("userName");
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <!-- Take argument from name, pass it to Hero and show it in h1 -->
     <Hero />
-    <h2>
-      Hello
-      <span class="u-name" v-if="name !== null || undefined">{{ name }}</span>
-    </h2>
-
-    <p v-if="name === null || undefined">What's your name?</p>
-    <input
-      type="text"
-      name="message"
-      id="message"
-      placeholder="Write your name here"
-      v-model="name"
-      autocomplete="off"
-    />
   </div>
 </template>
--- stores/index.ts
+++ stores/index.ts
@@ -1,14 +1,1 @@
 import { defineStore } from "@pinia/nuxt/dist/runtime/composables";
-
-export const useUNameStore = defineStore("userName", () => {
-  const name = ref<string>();
-  watch(name, (newName) => {
-    localStorage.setItem("userName", newName);
-  });
-
-  onMounted(() => {
-    name.value = localStorage.getItem("userName");
-  });
-
-  return { name };
-});
