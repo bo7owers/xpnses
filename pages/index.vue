a4d0f9befc4f0d0e4b08842774790e6245ee5c8b
bo7owers <rene7orres@gmail.com>
2022-08-02 21:22:03 -0400

    Add pages dir

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <!-- Take argument from name, pass it to Hero and show it in h1 -->
-    <NuxtLink to="/xpenses" />
+    <NuxtLink to="./xpenses" />
     <Hero />
     <NuxtPage />
   </div>
