d2f705fd2d5f2df08e55ddb86a6e3014b75473cb
bo7owers <7orres.rene@proton.me>
2022-11-20 10:06:49 -0500

    Update nuxt to stable version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
-import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["~/assets/fonts.css", "~/assets/styles.scss"],
-});
+    css: ['~/assets/fonts.css', '~/assets/styles.scss'],
+})
