cdfdc2da9839efece986f5258b78918e1310a194
bo7owers <7orres.rene@proton.me>
2022-11-26 22:02:40 -0500

    Install pinia with yarn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ['~/assets/fonts.css', '~/assets/styles.scss'],
+    modules: ['@pinia/nuxt'],
 })
